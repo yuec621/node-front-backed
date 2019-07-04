affc6b8cf041434ca2212b8e64ec05deb70939b6
yuec621 <yuec621@outlook.com>
2019-07-04 21:42:05 +0800

    英雄

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -91,9 +91,9 @@
                 class="avatar-uploader"
                 :action="$http.defaults.baseURL+'upload'"
                 :show-file-list="false"
-                :on-success="afterUpload"
+                :on-success="res=>$set(item,'icon',res.url)"
                 >
-                <img v-if="item.icon" :src="model.icon" class="avatar">
+                <img v-if="item.icon" :src="item.icon" class="avatar">
                 <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
         </el-form-item>
@@ -105,6 +105,11 @@
         <el-form-item label="小提示">
             <el-input v-model="item.tips" type="textarea" ></el-input>
         </el-form-item>
+
+  <el-form-item >
+            <el-button size="small" type="danger" @click="model.skills.splice(i,1)" >删除</el-button>
+        </el-form-item>
+
           </el-col>
 
         </el-row>
@@ -203,14 +208,14 @@ export default {
   .avatar-uploader-icon {
     font-size: 28px;
     color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
+    width: 5rem;
+    height: 5rem;
+    line-height: 5rem;
     text-align: center;
   }
   .avatar {
-    width: 178px;
-    height: 178px;
+    width: 5rem;
+    height: 5rem;
     display: block;
   }
 </style>
